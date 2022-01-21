c4ed4aa6a4cc95fa65ddcef12272fa62ec8eaf0b
Taha <taha.ojari@gmail.com>
2022-01-21 15:38:21 +0330

    feat: add loading and handling selected note in sidebar

--- features/Notes/components/Sidebar.tsx
+++ features/Notes/components/Sidebar.tsx
@@ -1,17 +1,26 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames';
+import Skeleton from 'react-loading-skeleton';
 import { ChevronLeftIcon } from '@heroicons/react/outline';
 import { useBreakpoints } from '../../../hooks/useBreakpoints';
 
 type Props = {
-  notes: {
-    key: number;
+  notes?: {
+    id: string;
     title: string;
     description: string;
   }[];
+  loading?: boolean;
+  selectedNote?: string;
+  onSelect: (id: string) => void;
 };
 
-const NotesSidebar = ({ notes }: Props): JSX.Element => {
+const NotesSidebar = ({
+  notes,
+  loading,
+  selectedNote,
+  onSelect,
+}: Props): JSX.Element => {
   const [open, setOpen] = useState(true);
   const { isLarge } = useBreakpoints();
 
@@ -27,7 +36,8 @@ const NotesSidebar = ({ notes }: Props): JSX.Element => {
     setOpen(!open);
   };
 
-  const handleNoteSelect = () => {
+  const handleNoteSelect = (id: string) => {
+    onSelect(id);
     if (!isLarge) {
       setOpen(false);
     }
@@ -56,16 +66,36 @@ const NotesSidebar = ({ notes }: Props): JSX.Element => {
       >
         <h3 className="text-3xl font-medium px-8">Notes</h3>
         <ul className="overflow-y-auto w-full divide-y">
-          {notes.map(({ key, title, description }) => (
-            <li
-              key={key}
-              className="py-4 px-8 hover:bg-gray-100 active:bg-gray-200 transition-colors duration-100 cursor-pointer"
-              onClick={handleNoteSelect}
-            >
-              <span className="text-xl font-medium">{title}</span>
-              <p className="truncate text-sm">{description}</p>
-            </li>
-          ))}
+          {!loading ? (
+            <>
+              {notes?.map(({ id, title, description }) => (
+                <li
+                  key={id}
+                  className={classNames(
+                    'py-4 px-8 transition-colors duration-100 cursor-pointer',
+                    {
+                      'bg-gray-200 active:bg-gray-300': id === selectedNote,
+                      'hover:bg-gray-100 active:bg-gray-200':
+                        id !== selectedNote,
+                    }
+                  )}
+                  onClick={() => handleNoteSelect(id)}
+                >
+                  <span className="text-xl font-medium">{title}</span>
+                  <p className="truncate text-sm">{description}</p>
+                </li>
+              ))}
+            </>
+          ) : (
+            <>
+              {[...Array(5)].map((_, index) => (
+                <div key={index} className="p-4">
+                  <Skeleton className="h-[24px] !w-[80px]" />
+                  <Skeleton className="h-[20px]" />
+                </div>
+              ))}
+            </>
+          )}
         </ul>
       </div>
     </div>
