5a9a470596001bd8fa155ae5796500361b89adde
Taha <taha@pragmateam.dev>
2022-01-21 10:25:16 +0330

    feat: init app using nextjs and tailwindcss 🐙

---
+++ features/Home/index.tsx
@@ -0,0 +1,7 @@
+import styles from './Home.module.css';
+
+const HomeFeature = (): JSX.Element => {
+  return <div className={styles.home}>Welcome to Note App</div>;
+};
+
+export default HomeFeature;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import HomeFeature from '../features/Home';
+
+const Home: NextPage = () => {
+  return <HomeFeature />;
+};
+
+export default Home;
