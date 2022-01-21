e41c1fb6491339d0aca841ecf1969aa10a8575c8
Taha <taha.ojari@gmail.com>
2022-01-21 15:56:34 +0330

    feat: use post to pass user to notes

--- hooks/useNotes/index.ts
+++ hooks/useNotes/index.ts
@@ -1,9 +1,16 @@
 import { useQuery } from 'react-query';
+import { useUser } from '../useUser';
 import { GetNotesResponse } from './@types';
 
 export const useNotes = () => {
+  const { user } = useUser();
+
   const { isLoading, data } = useQuery<GetNotesResponse>('notes', () =>
-    fetch('/api/note/notes').then((res) => res.json())
+    fetch('/api/note/notes', {
+      method: 'POST',
+      body: JSON.stringify({ user }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((res) => res.json())
   );
 
   return { isLoading, data };
