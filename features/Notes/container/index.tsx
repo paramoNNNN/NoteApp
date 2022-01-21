e78a26b4727c38525a2653ed22d33a3d1fea6c74
Taha <taha.ojari@gmail.com>
2022-01-21 15:39:09 +0330

    feat: use notes hook in notes-container

--- features/Notes/container/index.tsx
+++ features/Notes/container/index.tsx
@@ -1,25 +1,30 @@
+import { useState } from 'react';
+import { useNotes } from '../../../hooks/useNotes';
 import NoteEditor from '../components/Editor';
 import type { NoteEditorFields } from '../components/Editor/@types';
 import NotesSidebar from '../components/Sidebar';
 
-const notes = [
-  ...[...Array(10)].map((_, index) => ({
-    key: index,
-    title: `Note ${index + 1}`,
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Et aut veritatis natus dolorem, unde deleniti corrupti nihil blanditiis similique, soluta nostrum! Doloremque porro magnam nihil dolorem iusto. Aspernatur, ducimus quam',
-  })),
-];
-
 const NotesContainer = (): JSX.Element => {
+  const { isLoading, data } = useNotes();
+  const [selectedNote, setSelectedNote] = useState<string>();
+
   const handleSubmitNote = (fields: NoteEditorFields) => {
     console.log({ fields });
+  };
+
+  const handleNoteSelect = (id: string) => {
+    setSelectedNote(id);
   };
 
   return (
     <div className="h-screen w-screen flex justify-center items-center p-6">
       <div className="flex relative w-full md:w-5/6 h-full md:h-5/6 bg-white border border-gray-300 rounded-md shadow-sm">
-        <NotesSidebar notes={notes} />
+        <NotesSidebar
+          notes={data}
+          loading={isLoading}
+          selectedNote={selectedNote}
+          onSelect={handleNoteSelect}
+        />
 
         <NoteEditor onSubmit={handleSubmitNote} />
       </div>
