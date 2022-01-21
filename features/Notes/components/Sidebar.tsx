f75df88a5a41d8463e7abcb217722e7c88b24a4a
Taha <taha.ojari@gmail.com>
2022-01-21 17:38:15 +0330

    feat: add new note button to sidebar

--- features/Notes/components/Sidebar.tsx
+++ features/Notes/components/Sidebar.tsx
@@ -1,19 +1,16 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import Skeleton from 'react-loading-skeleton';
-import { ChevronLeftIcon } from '@heroicons/react/outline';
+import { ChevronLeftIcon, PlusIcon } from '@heroicons/react/outline';
 import { useBreakpoints } from '../../../hooks/useBreakpoints';
 import { Note } from '../../../pages/api/notes/@types';
 
 type Props = {
-  notes?: {
-    id: string;
-    title: string;
-    description: string;
-  }[];
+  notes?: Note[];
   loading?: boolean;
   selectedNote?: Note;
   onSelect: (id: Note) => void;
+  onNewNote: () => void;
 };
 
 const NotesSidebar = ({
@@ -21,6 +18,7 @@ const NotesSidebar = ({
   loading,
   selectedNote,
   onSelect,
+  onNewNote,
 }: Props): JSX.Element => {
   const [open, setOpen] = useState(true);
   const { isLarge } = useBreakpoints();
@@ -44,14 +42,42 @@ const NotesSidebar = ({
     }
   };
 
+  const handleNewNote = () => {
+    onNewNote();
+    if (!isLarge) {
+      setOpen(false);
+    }
+  };
+
+  const Notes = () => (
+    <>
+      {notes?.map(({ id, title, description }) => (
+        <li
+          key={id}
+          className={classNames(
+            'py-4 px-8 transition-colors duration-100 cursor-pointer',
+            {
+              'bg-gray-200 active:bg-gray-300': id === selectedNote?.id,
+              'hover:bg-gray-100 active:bg-gray-200': id !== selectedNote?.id,
+            }
+          )}
+          onClick={() => handleNoteSelect({ id, title, description })}
+        >
+          <span className="text-xl font-medium">{title}</span>
+          <p className="truncate text-sm">{description}</p>
+        </li>
+      ))}
+    </>
+  );
+
   return (
     <div
       className={`absolute lg:relative h-full ${
-        open ? 'w-full md:w-1/2 lg:w-3/12' : 'w-0'
-      } transition-all duration-200 bg-white rounded-md`}
+        open ? 'w-full md:w-1/2 lg:w-[300px] lg:min-w-[300px]' : 'w-0'
+      } transition-all duration-200 bg-white rounded-md z-10`}
     >
       <button
-        className="block lg:hidden absolute -right-4 top-5 p-1.5 w-8 h-8 rounded border border-gray-200 bg-white hover:bg-gray-100 transition-colors duration-100"
+        className="block lg:hidden absolute -right-4 top-[22px] p-1.5 w-8 h-8 rounded border border-gray-200 bg-white hover:bg-gray-100 transition-colors duration-100"
         onClick={toggleSidebar}
       >
         <ChevronLeftIcon
@@ -65,28 +91,19 @@ const NotesSidebar = ({
             : 'border-transparent'
         }`}
       >
-        <h3 className="text-3xl font-medium px-8">Notes</h3>
+        <div className="flex items-center justify-between pr-8">
+          <h3 className="text-3xl font-medium px-8">Notes</h3>
+
+          <button
+            className="border border-gray-200 hover:bg-gray-100 active:bg-gray-200 p-1.5 w-8 h-8 rounded shadow-sm transition-colors duration-200"
+            onClick={handleNewNote}
+          >
+            <PlusIcon />
+          </button>
+        </div>
         <ul className="overflow-y-auto w-full divide-y">
           {!loading ? (
-            <>
-              {notes?.map(({ id, title, description }) => (
-                <li
-                  key={id}
-                  className={classNames(
-                    'py-4 px-8 transition-colors duration-100 cursor-pointer',
-                    {
-                      'bg-gray-200 active:bg-gray-300': id === selectedNote?.id,
-                      'hover:bg-gray-100 active:bg-gray-200':
-                        id !== selectedNote?.id,
-                    }
-                  )}
-                  onClick={() => handleNoteSelect({ id, title, description })}
-                >
-                  <span className="text-xl font-medium">{title}</span>
-                  <p className="truncate text-sm">{description}</p>
-                </li>
-              ))}
-            </>
+            <Notes />
           ) : (
             <>
               {[...Array(5)].map((_, index) => (
