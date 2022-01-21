200dd60973395ad22588fb8ec074a08a0919c62d
Taha <taha.ojari@gmail.com>
2022-01-21 11:36:47 +0330

    feat: create login container

--- features/Login/container/index.tsx
+++ features/Login/container/index.tsx
@@ -1,5 +1,26 @@
+import { useRouter } from 'next/router';
+import { useUser } from '../../../hooks/useUser';
+import { LoginFormFields } from '../@types';
+import LoginForm from '../components/LoginForm';
+
 const LoginContainer = (): JSX.Element => {
-  return <div>Login Container</div>;
+  const { setUser } = useUser();
+  const { push } = useRouter();
+
+  const handleSubmit = ({ username }: LoginFormFields) => {
+    setUser(username);
+    push('/');
+  };
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center p-4 sm:p-6 md:p-8">
+      <div className="w-full sm:w-auto space-y-10 p-5 bg-white border border-gray-300 rounded-md shadow-sm">
+        <h3 className="text-2xl font-semibold">Login</h3>
+
+        <LoginForm onSubmit={handleSubmit} />
+      </div>
+    </div>
+  );
 };
 
 export default LoginContainer;
